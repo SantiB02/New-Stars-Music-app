2999775ac8dc435c5b8d819af13d1149d5bbf508
SantiB02 <santibrasca02@gmail.com>
2024-04-10 18:39:06 -0300

    Instalar Tailwindcss y crear componentes base

---
+++ Frontend/New-Stars-Music-app/src/components/home/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <div className="flex flex-col">
+        <h1 className="text-4xl">Welcome to New Stars Music!</h1>
+        <p>
+          The place where you can listen to your favorite songs and explore
+          their artists' products in our all-in-one store.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
