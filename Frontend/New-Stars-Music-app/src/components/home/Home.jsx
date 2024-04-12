30edd8b3e9d9dac19e413c5c79a36efaab1d0144
Alvaro Johansen <109972764+Alvarojohansen@users.noreply.github.com>
2024-04-12 20:45:31 -0300

    added Footer

--- Frontend/New-Stars-Music-app/src/components/home/Home.jsx
+++ Frontend/New-Stars-Music-app/src/components/home/Home.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import NavBar from "../NavBar";
+import Footer from "../Footer";
 
 const Home = () => {
   return (
-    <>
-    <NavBar/>
-      <div>
-        <div className="flex flex-col">
+    <div className="flex flex-col min-h-screen">
+      <NavBar />
+      <div className="flex-grow overflow-auto">
+        <div className="flex flex-col mt-10">
           <h1 className="text-4xl">Welcome to New Stars Music!</h1>
           <p>
             The place where you can listen to your favorite songs and explore
@@ -14,7 +15,8 @@ const Home = () => {
           </p>
         </div>
       </div>
-    </>
+      <Footer />
+    </div>
   );
 };
 
