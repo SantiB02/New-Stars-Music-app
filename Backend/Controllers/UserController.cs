5532838f8110ad272033cd77058a30d31332a6fe
CoriaEzequiel <zequielcoria@gmail.com>
2024-04-27 11:08:37 -0300

    base Interface, Implementation and Controller of User created
    to continued

---
+++ Backend/Controllers/UserController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Merchanmusic.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IUserService _userService;
+        public UserController (IUserService userService) { }
+    }
+}
---
+++ Backend/Services/Interfaces/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using Merchanmusic.Data.Entities;
+using Merchanmusic.Data.Models;
+
+public interface IUserService
+{
+    public User? GetUserByEmail(string email);
+    public bool CheckIfUserExists(string userEmail);
+    public BaseResponse ValidateUser(string email, string password);
+    public int CreateUser(User user);
+ //  public Updated UpdateUser(User user);
+//   public Deleted DeleteUser(int userId);
+    public List<User> GetUsersByRole(string role);
+}
