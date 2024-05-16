dcb67568f7937afb8dc56be8080aaac1a9b1e2fc
SantiB02 <santibrasca02@gmail.com>
2024-05-16 01:14:14 -0300

    Create LoadingMessage component (skeleton)

---
+++ Frontend/New-Stars-Music-app/src/components/common/LoadingMessage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LoadingMessage = ({ message }) => {
+  return (
+    <div>
+      {/* SPINNER GIF CON ALGÚN MENSAJITO DEPENDIENDO LO QUE ESTÁ ESPERANDO */}
+    </div>
+  );
+};
+
+export default LoadingMessage;
