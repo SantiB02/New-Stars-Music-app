9026a3bdbc99a3c5682022e3ebc86a33561b9d07
novoluisina <novoluisina@gmail.com>
2024-06-02 22:20:20 -0300

    controller update

--- Backend/Controllers/SaleOrderLineController.cs
+++ Backend/Controllers/SaleOrderLineController.cs
@@ -39,5 +39,29 @@ namespace Merchanmusic.Controllers
             }
             return Forbid();
         }
+
+        [HttpGet("GetAllByProduct/{productId}")]
+        public IActionResult GetAllByProduct([FromRoute] int productId)
+        {
+            string role = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Role).Value.ToString();
+            if (role == "Admin")
+            {
+                try
+                {
+                    var sol = _solService.GetAllByProduct(productId);
+                    if (sol.Count == 0)
+                    {
+                        return NotFound("Líneas de venta no encontradas");
+                    }
+                    return Ok(sol);
+
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest($"Error: {ex.Message}");
+                }
+            }
+            return Forbid();
+        }
     }
 }
