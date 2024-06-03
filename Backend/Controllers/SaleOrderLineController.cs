1eb0c05b4fa51b6591624501201f86498444cb68
novoluisina <novoluisina@gmail.com>
2024-06-02 22:12:35 -0300

    sale order line controller

---
+++ Backend/Controllers/SaleOrderLineController.cs
@@ -0,0 +1,43 @@
+﻿using Merchanmusic.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Merchanmusic.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SaleOrderLineController : ControllerBase
+    {
+        private readonly ISaleOrderLineService _solService;
+
+        public SaleOrderLineController(ISaleOrderLineService solService)
+        {
+            _solService = solService;
+        }
+
+        [HttpGet("GetAllBySaleOrder/{saleOrderId}")]
+        public IActionResult GetAllBySaleOrder([FromRoute] int saleOrderId)
+        {
+            string role = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Role).Value.ToString();
+            if (role == "Admin")
+            {
+                try
+                {
+                    var sol = _solService.GetAllBySaleOrder(saleOrderId);
+                    if (sol.Count == 0)
+                    {
+                        return NotFound("Líneas de venta no encontradas");
+                    }
+                    return Ok(sol);
+
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest($"Error: {ex.Message}");
+                }
+            }
+            return Forbid();
+        }
+    }
+}
