b71e5ee34ab878016f8168f6c7fb6c93932cc0c3
novoluisina <novoluisina@gmail.com>
2024-06-10 14:36:03 -0300

    Product interface

---
+++ Backend/Services/Interfaces/IProductService.cs
@@ -0,0 +1,15 @@
+﻿using Merchanmusic.Data.Entities.Products;
+
+namespace Merchanmusic.Services.Interfaces
+{
+        public interface IProductService
+        {
+            List<Product> GetProducts();
+            Product? GetProductById(int id);
+            Product? GetProductByName(string name);
+            int CreateProduct(Product product);
+            void DeleteProduct(int id);
+            Product UpdateProduct(Product product);
+        }
+
+}
