6ffab1c53380b0a8c7e672b6fad671850b9419a5
SantiB02 <santibrasca02@gmail.com>
2024-06-11 20:50:11 -0300

    Get Auth0 settings from our backend

--- Frontend/New-Stars-Music-app/src/main.jsx
+++ Frontend/New-Stars-Music-app/src/main.jsx
@@ -3,16 +3,19 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { Auth0Provider } from "@auth0/auth0-react";
+import { getAuthSettings } from "./api/settings-api.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <Auth0Provider
-      domain="dev-a64glq5ygldhuy1g.us.auth0.com"
-      clientId="tlfvwom9x9e6ylmyXrLxxKYhth1hKA5l"
-      authorizationParams={{ redirect_uri: window.location.origin }}
-    >
-      <App />
-    </Auth0Provider>
-  </React.StrictMode>
-);
+getAuthSettings().then((authSettings) => {
+  root.render(
+    <React.StrictMode>
+      <Auth0Provider
+        domain={authSettings.domain}
+        clientId={authSettings.clientId}
+        authorizationParams={{ redirect_uri: window.location.origin }}
+      >
+        <App />
+      </Auth0Provider>
+    </React.StrictMode>
+  );
+});
