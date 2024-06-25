75c56d1342d18cec1d7a28cbaebab05774e4b676
SantiB02 <santibrasca02@gmail.com>
2024-06-25 16:04:17 -0300

    add placeholder image to products

--- Frontend/New-Stars-Music-app/src/api/settings-api.js
+++ Frontend/New-Stars-Music-app/src/api/settings-api.js
@@ -3,7 +3,6 @@ import api from "./api";
 export const getAuthSettings = async () => {
   try {
     const response = await api.get("/Settings/auth");
-    console.log("response", response);
     return response.data;
   } catch (error) {
     console.error("There was an error fetching auth results", error);
