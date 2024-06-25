9975c354031ff94ffe13d2d642715485da40aa4b
Alvarojohansen <johansenalvaro@gmail.com>
2024-06-25 00:18:23 -0300

    added the ThemeContext and toggle custom

--- Frontend/New-Stars-Music-app/src/main.jsx
+++ Frontend/New-Stars-Music-app/src/main.jsx
@@ -4,18 +4,21 @@ import App from "./App.jsx";
 import "./index.css";
 import { Auth0Provider } from "@auth0/auth0-react";
 import { getAuthSettings } from "./api/settings-api.js";
+import { ThemeContextProvider } from "./services/contexts/ThemeProvider.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 getAuthSettings().then((authSettings) => {
   root.render(
     <React.StrictMode>
-      <Auth0Provider
-        domain={authSettings.domain}
-        clientId={authSettings.clientId}
-        authorizationParams={{ redirect_uri: window.location.origin }}
-      >
-        <App />
-      </Auth0Provider>
+      <ThemeContextProvider>
+        <Auth0Provider
+          domain={authSettings.domain}
+          clientId={authSettings.clientId}
+          authorizationParams={{ redirect_uri: window.location.origin }}
+        >
+          <App />
+        </Auth0Provider>
+      </ThemeContextProvider>
     </React.StrictMode>
   );
 });
