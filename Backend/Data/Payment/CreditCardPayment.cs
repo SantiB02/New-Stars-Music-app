1dd447eea2830c458e2413da2d8e38cb8f58e11d
novoluisina <novoluisina@gmail.com>
2024-07-11 19:40:45 -0300

    factory

---
+++ Backend/Data/Payment/CreditCardPayment.cs
@@ -0,0 +1,12 @@
+﻿using Merchanmusic.Services.Interfaces;
+
+namespace Merchanmusic.Data.Payment
+{
+    public class CreditCardPayment : IPayment
+    {
+        public void ProcessPayment(decimal amount)
+        {
+            Console.WriteLine($"Processing credit card payment of {amount}.");
+        }
+    }
+}
---
+++ Backend/Migrations/20240711221514_datab.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Merchanmusic.Migrations
+{
+    public partial class datab : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "CreationDate", "LastModifiedDate" },
+                values: new object[] { new DateTime(2024, 7, 11, 19, 15, 13, 635, DateTimeKind.Local).AddTicks(8655), new DateTime(2024, 7, 11, 19, 15, 13, 635, DateTimeKind.Local).AddTicks(8666) });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "CreationDate", "LastModifiedDate" },
+                values: new object[] { new DateTime(2024, 7, 11, 19, 15, 13, 635, DateTimeKind.Local).AddTicks(8676), new DateTime(2024, 7, 11, 19, 15, 13, 635, DateTimeKind.Local).AddTicks(8677) });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 5,
+                columns: new[] { "CreationDate", "LastModifiedDate" },
+                values: new object[] { new DateTime(2024, 7, 11, 19, 15, 13, 635, DateTimeKind.Local).AddTicks(8680), new DateTime(2024, 7, 11, 19, 15, 13, 635, DateTimeKind.Local).AddTicks(8681) });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 7,
+                columns: new[] { "CreationDate", "LastModifiedDate" },
+                values: new object[] { new DateTime(2024, 7, 11, 19, 15, 13, 635, DateTimeKind.Local).AddTicks(8684), new DateTime(2024, 7, 11, 19, 15, 13, 635, DateTimeKind.Local).AddTicks(8684) });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "CreationDate", "LastModifiedDate" },
+                values: new object[] { new DateTime(2024, 7, 9, 19, 47, 23, 510, DateTimeKind.Local).AddTicks(8600), new DateTime(2024, 7, 9, 19, 47, 23, 510, DateTimeKind.Local).AddTicks(8612) });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "CreationDate", "LastModifiedDate" },
+                values: new object[] { new DateTime(2024, 7, 9, 19, 47, 23, 510, DateTimeKind.Local).AddTicks(8621), new DateTime(2024, 7, 9, 19, 47, 23, 510, DateTimeKind.Local).AddTicks(8621) });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 5,
+                columns: new[] { "CreationDate", "LastModifiedDate" },
+                values: new object[] { new DateTime(2024, 7, 9, 19, 47, 23, 510, DateTimeKind.Local).AddTicks(8624), new DateTime(2024, 7, 9, 19, 47, 23, 510, DateTimeKind.Local).AddTicks(8624) });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 7,
+                columns: new[] { "CreationDate", "LastModifiedDate" },
+                values: new object[] { new DateTime(2024, 7, 9, 19, 47, 23, 510, DateTimeKind.Local).AddTicks(8627), new DateTime(2024, 7, 9, 19, 47, 23, 510, DateTimeKind.Local).AddTicks(8627) });
+        }
+    }
+}
