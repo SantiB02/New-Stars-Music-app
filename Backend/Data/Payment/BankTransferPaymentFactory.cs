19bd6545b3493ab3ea376669ae8b50ee9697cb75
novoluisina <novoluisina@gmail.com>
2024-07-11 19:59:45 -0300

    bank

---
+++ Backend/Data/Payment/BankTransferPaymentFactory.cs
@@ -0,0 +1,12 @@
+﻿using Merchanmusic.Services.Interfaces;
+
+namespace Merchanmusic.Data.Payment
+{
+    public class BankTransferPaymentFactory : PaymentFactory
+    {
+        public override IPayment CreatePayment()
+        {
+            return new BankTransferPayment();
+        }
+    }
+}
